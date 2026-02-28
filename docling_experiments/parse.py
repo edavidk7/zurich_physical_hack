409ebe0ea7e0222a9ee227b93b13318ec542d159
Jekaterina Jaroslavceva <jekatrinaj@microsoft.com>
2026-02-28 13:48:24 +0100

    Remove docling_experiments

--- docling_experiments/parse.py
+++
@@ -1,8 +0,0 @@
-from docling.document_converter import DocumentConverter
-import json
-
-source = "sample_docs/bq79600-q1.pdf"
-converter = DocumentConverter()
-doc = converter.convert(source).document
-with open("test.json", "w") as t:
-    json.dump(doc.export_to_dict(), t)
